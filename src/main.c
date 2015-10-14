c262562336fd67b7dffc59c1a29a7ee29bda53c8
Alexei Colin <ac@alexeicolin.com>
2015-10-14 19:41:16 -0400

    draft of normalization of product

--- src/main.c
+++ src/main.c
@@ -35,9 +35,21 @@
 // If you link-in wisp-base, then you have to define some symbols.
 uint8_t usrBank[USRBANK_SIZE];
 
+// TODO: this is app-specific, but a good candidate for genericizing
+// This pertains to the pattern of reusable tasks using a 'next task' value.
+typedef enum {
+    TASK_NORMALIZABLE,
+    TASK_REDUCE,
+    // Not all tasks listed because only some are used as next task
+} task_t;
+
 struct msg_mult {
     CHAN_FIELD_ARRAY(uint16_t, A, NUM_DIGITS);
     CHAN_FIELD_ARRAY(uint16_t, B, NUM_DIGITS);
+};
+
+struct msg_modulus {
+    CHAN_FIELD_ARRAY(uint16_t, M, NUM_DIGITS);
 };
 
 struct msg_digit {
@@ -49,14 +61,28 @@ struct msg_product {
     CHAN_FIELD_ARRAY(uint16_t, product, NUM_DIGITS * 2);
 };
 
+struct msg_next_task {
+    CHAN_FIELD(task_t, next_task);
+};
+
 TASK(0, task_init)
 TASK(1, task_mult)
-TASK(2, task_print)
+TASK(2, task_normalizable)
+TASK(3, task_normalize)
+TASK(4, task_reduce)
+TASK(5, task_print_product)
 
 CHANNEL(task_init, task_mult, msg_digit);
-MULTICAST_CHANNEL(msg_mult, mult_args, task_init, task_mult, task_print);
+MULTICAST_CHANNEL(msg_modulus, ch_modulus, task_init, task_normalizable, task_normalize);
+MULTICAST_CHANNEL(msg_mult, ch_mult_args, task_init, task_mult, task_print_product);
 SELF_CHANNEL(task_mult, msg_digit);
-CHANNEL(task_mult, task_print, msg_product);
+MULTICAST_CHANNEL(msg_product, ch_product, task_mult,
+                  task_normalizable, task_normalize, task_print_product);
+MULTICAST_CHANNEL(msg_product, ch_normalized_product, task_normalize,
+                  task_reduce, task_print_product);
+CHANNEL(task_mult, task_print_product, msg_next_task);
+CHANNEL(task_normalizable, task_print_product, msg_next_task);
+CHANNEL(task_normalize, task_print_product, msg_next_task);
 
 void init()
 {
@@ -106,7 +132,7 @@ static void blink(unsigned count, uint32_t duration, unsigned leds)
 void task_init()
 {
     int i;
-    uint16_t a, b;
+    uint16_t d;
 
     printf("init\r\n");
 
@@ -115,16 +141,23 @@ void task_init()
     // test values
     printf("init: A=");
     for (i = NUM_DIGITS - 1; i >= 0; --i) {
-        a = (0x10 + (0x10 * i)) & REG_MASK;
-        CHAN_OUT(A[i], a, MC_OUT_CH(mult_args, task_init, task_mult, task_print));
-        printf("%x ", a);
+        d = (0x10 + (0x10 * i)) & REG_MASK;
+        CHAN_OUT(A[i], d, MC_OUT_CH(ch_mult_args, task_init, task_mult, task_print));
+        printf("%x ", d);
     }
     printf("\r\n");
     printf("init: B=");
     for (i = NUM_DIGITS - 1; i >= 0; --i) {
-        b = (0x80 + (0x10 * i)) & REG_MASK;
-        CHAN_OUT(B[i], b, MC_OUT_CH(mult_args, task_init, task_mult, task_print));
-        printf("%x ", b);
+        d = (0x80 + (0x10 * i)) & REG_MASK;
+        CHAN_OUT(B[i], d, MC_OUT_CH(ch_mult_args, task_init, task_mult, task_print));
+        printf("%x ", d);
+    }
+    printf("\r\n");
+    printf("init: M=");
+    for (i = NUM_DIGITS - 1; i >= 0; --i) {
+        d = (0x08 + (0x10 * i)) & REG_MASK;
+        CHAN_OUT(M[i], d, MC_OUT_CH(ch_modulus, task_init, task_normalizable, task_normalize));
+        printf("%x ", d);
     }
     printf("\r\n");
 
@@ -151,8 +184,8 @@ void task_mult()
     p = carry;
     for (i = 0; i < NUM_DIGITS; ++i) {
         if (digit - i >= 0 && digit - i < NUM_DIGITS) {
-            a = *CHAN_IN1(A[digit - i], MC_IN_CH(mult_args, task_init, task_mult));
-            b = *CHAN_IN1(B[i], MC_IN_CH(mult_args, task_init, task_mult));
+            a = *CHAN_IN1(A[digit - i], MC_IN_CH(ch_mult_args, task_init, task_mult));
+            b = *CHAN_IN1(B[i], MC_IN_CH(ch_mult_args, task_init, task_mult));
             p += a * b;
             printf("mult: i=%u a=%x b=%x p=%x\r\n", i, a, b, p);
         }
@@ -163,7 +196,8 @@ void task_mult()
 
     printf("mult: c=%x p=%x\r\n", c, p);
 
-    CHAN_OUT(product[digit], p, CH(task_mult, task_print));
+    CHAN_OUT(product[digit], p, MC_OUT_CH(ch_product, task_mult,
+             task_normalizable, task_normalize, task_print_product));
 
     digit++;
 
@@ -172,25 +206,127 @@ void task_mult()
         CHAN_OUT(digit, digit, SELF_OUT_CH(task_mult));
         TRANSITION_TO(task_mult);
     } else {
-        TRANSITION_TO(task_print);
+        CHAN_OUT(next_task, TASK_NORMALIZABLE, CH(task_mult, task_print_product));
+        TRANSITION_TO(task_print_product);
     }
 }
 
-void task_print()
+void task_normalizable()
+{
+    int i;
+    unsigned p, m;
+    bool normalizable = true;
+
+    // Variables:
+    //   l: number of digits in the product (2 * NUM_DIGITS)
+    //   k: number of digits in the modulus (NUM_DIGITS)
+    //
+    // if (x > m b^(l-k)
+    //     x = x - m b^(l-k)
+    //
+    // NOTE: It's temptimg to do the subtraction opportunistically, and if
+    // the result is negative, then the condition must have been false.
+    // However, we can't do that because under this approach, we must
+    // write to the output channel zero digits for digits that turn
+    // out to be equal, but if a later digit pair is such that condition
+    // is false (p < m), then those rights are invalid, but we have no
+    // good way of exluding them from being picked up by the later
+    // task. One possiblity is to transmit a flag to that task that
+    // tells it whether to include our output channel into its input sync
+    // statement. However, this seems less elegant than splitting the
+    // normalization into two tasks: the condition and the subtraction.
+    //
+    // Multiplication by a power of radix (b) is a shift, so when we do the
+    // comparison/subtraction of the digits, we offset the index into the
+    // product digits by (l-k) = NUM_DIGITS.
+
+    for (i = NUM_DIGITS - 1; i >= 0; --i) {
+        p = *CHAN_IN1(product[i * 2], MC_IN_CH(ch_product, task_mult, task_normalizable));
+        m = *CHAN_IN1(M[i], MC_IN_CH(ch_modulus, task_init, task_normalizable));
+
+        if (p > m) {
+            normalizable = false;
+            break;
+        }
+    }
+
+    printf("normalizable: %u\r\n", normalizable);
+
+    if (normalizable) {
+        TRANSITION_TO(task_normalize);
+    } else {
+        CHAN_OUT(next_task, TASK_REDUCE, CH(task_normalizable, task_print_product));
+        TRANSITION_TO(task_print_product);
+    }
+}
+
+// TODO: consider decomposing into subtasks
+void task_normalize()
+{
+    int i;
+    uint16_t p, m, d;
+
+    for (i = NUM_DIGITS - 1; i >= 0; --i) {
+        p = *CHAN_IN1(product[i * 2], MC_IN_CH(ch_product, task_mult, task_normalize));
+        m = *CHAN_IN1(M[i], MC_IN_CH(ch_modulus, task_init, task_normalize));
+
+        d = p - m;
+        CHAN_OUT(product[i * 2], d, MC_OUT_CH(ch_normalized_product, task_normalize,
+                                              task_reduce, task_print_product));
+    }
+
+#if 0 // NOTE: we don't need to do that because: sync! it grabs the latest
+    // Copy the digits unaffected by the normalization
+    for (i = NUM_DIGITS - 1; i >= 0; --i) {
+        d = *CHAN_IN1(product[i], MC_IN_CH(task_mult, task_normalize));
+        CHAN_OUT(product[i], d, CH(task_normalize, task_reduce));
+    }
+#endif
+
+    CHAN_OUT(next_task, TASK_REDUCE, CH(task_normalize, task_print_product));
+    TRANSITION_TO(task_print_product);
+}
+
+void task_reduce()
+{
+    blink(1, SEC_TO_CYCLES, LED2);
+
+    // TODO: CHAN_IN2(product[], MC_IN_CH(ch_product, task_mult),
+    //                           MC_IN_CH(ch_normalized_product, task_normalize));
+
+    TRANSITION_TO(task_init);
+}
+
+void task_print_product()
 {
     int i;
     uint16_t p;
-
-    blink(1, SEC_TO_CYCLES, LED2);
+    task_t next_task;
 
     printf("print: P=");
     for (i = (NUM_DIGITS * 2) - 1; i >= 0; --i) {
-        p = *CHAN_IN1(product[i], CH(task_mult, task_print));
+        p = *CHAN_IN2(product[i], MC_IN_CH(ch_product, task_mult, task_print_product),
+                                  MC_IN_CH(ch_normalized_product, task_normalize, task_print_product));
         printf("%x ", p);
     }
     printf("\r\n");
 
-    TRANSITION_TO(task_init);
+    next_task = *CHAN_IN2(next_task, CH(task_mult, task_print_product),
+                                     CH(task_normalize, task_print_product));
+
+    // TODO: this is where first-class tasks would come in useful. Basically,
+    // any re-usable task would have this control flow branching.
+    switch (next_task) {
+        case TASK_NORMALIZABLE:
+            TRANSITION_TO(task_normalizable);
+            break;
+        case TASK_REDUCE:
+            TRANSITION_TO(task_reduce);
+            break;
+        default:
+            // TODO: ABORT
+            break;
+    }
 }
 
 ENTRY_TASK(task_init)
